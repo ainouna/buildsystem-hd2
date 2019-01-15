5532d26e962edf8ed88f385d682f1d6924108439
mohousch <mohousch@gmail.com>
2019-01-15 15:40:11 +0100

    - build default with libfribidi.

--- make/neutrino.mk
+++ make/neutrino.mk
@@ -379,7 +379,6 @@ $(D)/neutrino-hd2.config.status:
 			--with-plugindir=/var/tuxbox/plugins \
 			$(NHD2_OPTS) \
 			--enable-scart \
-			--enable-fribidi \
 			PKG_CONFIG=$(PKG_CONFIG) \
 			PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) \
 			CPPFLAGS="$(N_CPPFLAGS)" LDFLAGS="$(TARGET_LDFLAGS)"
