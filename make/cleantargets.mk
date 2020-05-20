3e0e5c95a154b6f462109cc99419f4ac2b6ed193
Mhamed Saib <mohousch@debian>
2020-05-20 19:07:17 +0200

    make clean: dont clean crosstool.

--- make/cleantargets.mk
+++ make/cleantargets.mk
@@ -6,7 +6,6 @@ clean: depsclean
 	@-$(MAKE) kernel-clean
 	@-$(MAKE) tools-clean
 	@-$(MAKE) driver-clean
-	@-rm -rf $(BASE_DIR)/tufsbox
 	@-rm -rf $(D)/kernel
 	@-rm -rf $(D)/*.do_compile
 	@-rm -rf $(D)/*.config.status
