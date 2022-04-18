c69603277e5e1d242986e292ec630e247f4fa61d
soap-bubble-coin <94795315+soap-bubble-coin@users.noreply.github.com>
2022-04-18 11:55:58 +0300

    Add files via upload

--- make/release-neutrino.mk
+++ make/release-neutrino.mk
@@ -2,10 +2,10 @@
 # release-neutrino
 #
 release-neutrino: $(D)/neutrino $(D)/neutrino-plugins release-none
-	cp -af $(TARGET_DIR)/usr/local/bin $(RELEASE_DIR)/usr/local/
-	cp -dp $(TARGET_DIR)/.version $(RELEASE_DIR)/
-	cp -aR $(TARGET_DIR)/var/tuxbox/* $(RELEASE_DIR)/var/tuxbox
-	cp -aR $(TARGET_DIR)/usr/share/tuxbox/* $(RELEASE_DIR)/usr/share/tuxbox
+	cp -rf $(TARGET_DIR)/usr/local/bin $(RELEASE_DIR)/usr/local/
+	cp -rf $(TARGET_DIR)/.version $(RELEASE_DIR)/
+	cp -rf $(TARGET_DIR)/var/tuxbox/* $(RELEASE_DIR)/var/tuxbox
+	cp -rf $(TARGET_DIR)/usr/share/tuxbox/* $(RELEASE_DIR)/usr/share/tuxbox
 #
 # linux-strip all
 #
